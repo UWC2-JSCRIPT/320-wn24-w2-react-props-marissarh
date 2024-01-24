1c602dec59c86eb5f9405d65f2d85fdadd390f45
marissarh <mbutto26@gmail.com>
2024-01-24 12:31:14 -0800

    update

--- blog-homepage/src/App.jsx
+++ blog-homepage/src/App.jsx
@@ -1,100 +1,17 @@
 import Picture from './Picture'
-import Article from './Article'
+import Article, { article } from './Article'
 import './App.css'
 
+  
+export default function List(){
 
+  const articleList = article.map(Title => <li>{Title}</li>);
+    return <ul>{articleList}</ul>
+}
 
-
- 
-
-  const article=[{
-    id: 0,
-    img0: "./images/painting.webp",
-    artTitle:'The 7 Habits of Highly Creative People',
-    artDescr:'What a good artist understand is that nothing comes from nowhere. Nothing is completely original.',
-    author: 'Louis Chew',
-    artLength:'8 min read',
-    artDate:'Apr 4',
-
-  },
-{
-  id:1,
-  img1:"./images/graffiti.avif",
-  artTitle:`Do Something You Can't Win At`,
-  artDescr:'And make it something you love',
-  author:'Kris Gage',
-  artLength:'3 min read',
-  artDate:'Mar 30',
-},{
-  id:2,
-  img2:"./images/electricChart.jpg",
-  artTitle:`This Handy Chart Automatically Generates a Pitch for Your New Novel`,
-  artDescr:'Feeling stuck? We help you skip the writing and go traight to the press release',
-  author:'Electric Literature',
-  artLength:'2 min read',
-  artDate:'Mar 28',
-},
-{
-  id:3,
-  img3:"./images/paddleBall.png",
-  artTitle:`Art as a Second Language`,
-  artDescr:'Drawing is a translation of all my voices and words — an attempt to draw nothing',
-  author:'Roman Muradov',
-  artLength:'6 min read',
-  artDate:'Mar 27',
-},
-{
-  id:4,
-  img4:"./images/blackSmudge.jpg",
-  artTitle:`What Would Happen If You Did These Things Every Day...`,
-  artDescr:'For thirty days...',
-  author:'Alex Mathers',
-  artLength:'3 min read',
-  artDate:'Apr 22',
-},
-{
-  id:5,
-  img5:"./images/typewriter.jpg",
-  artTitle:'10 Strategies for Creating Viral and Powerful Content',
-  author:'Zdravko Cvijetic',
-  artLength:'6 min read',
-  artDate:'Apr 22'
-},
-{id:6,
-  img7:"./images/papers.jpg",
-  artTitle:`Useful Tools for Writers`,
-  artDescr:'Jumpstart you process.',
-  author:'JLRose',
-  artLength:'8 min read',
-  artDate:'Apr 21',
-
-}]
- 
-
-  const alt="Person painting"
-  const alt2="Swings in front of graffiti wall"
-  const alt3="Electric Literature's Auto-Publicist"
-  const alt4="Sketch of a woman playing paddle-ball"
-  const alt5="Three rows of black smudges"
-  const alt6="A red typewriter"
-  const alt7="Messy pile of papers"
-
-const authPic1="./images/LouisChew.jpg"
-const authPic2="./images/KrisGage.png"
-const authPic3="./images/electricLiterature.jpg"
-const authPic4="./images/RomanMuradov.png"
-const authPic5="./images/AlexMathers.jpg"
-const authPic6="./images/ZdravkoCivjetic.jpg"
-const authPic7=""
-
-const sec1="For You"
+/*const sec1="For You"
 const sec2="In case you missed it"
 const sec3 = "bookmark"
-const sec4= "Audio available"
+const sec4= "Audio available"*/
 
 
-export default function Article(){
-  const articleList = article.map.articles => 
-  <li>{Article}</li>;
-  return <ul> {articleLists}</ul>;
-}
--- blog-homepage/src/Article.jsx
+++ blog-homepage/src/Article.jsx
@@ -1,8 +1,90 @@
-import Picture from './Picture';
+
 import App from './App';
 import './App.css'
 
 
+
+export const article=[
+    {
+      id: 0,
+      image: "./images/painting.webp",
+      imageAlt: "Person painting",
+      Title:'The 7 Habits of Highly Creative People',
+      Descr:'What a good artist understand is that nothing comes from nowhere. Nothing is completely original.',
+      authPic:"./images/LouisChew.jpg",
+      author: 'Louis Chew',
+      Length:'8 min read',
+      Date:'Apr 4',
+  
+    },
+  {
+    id:1,
+    img1:"./images/graffiti.avif",
+    imageAlt: "Swings in front of graffiti wall",
+    Title:`Do Something You Can't Win At`,
+    authPic:"./images/KrisGage.png",
+    Descr:'And make it something you love',
+    author:'Kris Gage',
+    Length:'3 min read',
+    Date:'Mar 30',
+  },{
+    id:2,
+    img2:"./images/electricChart.jpg",
+    imageAlt: "Electric Literature's Auto-Publicist",
+    Title:`This Handy Chart Automatically Generates a Pitch for Your New Novel`,
+    Descr:'Feeling stuck? We help you skip the writing and go traight to the press release',
+    authPic:"./images/electricLiterature.jpg",
+    author:'Electric Literature',
+    Length:'2 min read',
+    Date:'Mar 28',
+  },
+  {
+    id:3,
+    img3:"./images/paddleBall.png",
+    Title:`Art as a Second Language`,
+    imageAlt: "Sketch of a woman playing paddle-ball",
+    Descr:'Drawing is a translation of all my voices and words — an attempt to draw nothing',
+    authPic:"./images/RomanMuradov.png",
+    author:'Roman Muradov',
+    Length:'6 min read',
+    Date:'Mar 27',
+  },
+  {
+    id:4,
+    img4:"./images/blackSmudge.jpg",
+    imageAlt: "Three rows of black smudges",
+    Title:`What Would Happen If You Did These Things Every Day...`,
+    Descr:'For thirty days...',
+    authPic:"./images/AlexMathers.jpg",
+    author:'Alex Mathers',
+    Length:'3 min read',
+    Date:'Apr 22',
+  },
+  {
+    id:5,
+    img5:"./images/typewriter.jpg",
+    imageAlt: "A red typewriter",
+    Title:'10 Strategies for Creating Viral and Powerful Content',
+    authPic:"./images/ZdravkoCivjetic.jpg",
+    author:'Zdravko Cvijetic',
+    Length:'6 min read',
+    Date:'Apr 22'
+  },
+  {id:6,
+    img7:"./images/papers.jpg",
+    imageAlt: "Messy pile of papers",
+    Title:`Useful Tools for Writers`,
+    Descr:'Jumpstart you process.',
+    author:'JLRose',
+    Length:'8 min read',
+    Date:'Apr 21',
+  
+  }]
+  /*return(
+    <>
+    <ul>{Article}</ul>
+    </>
+  );
 function Article({ artTitle,artDescr, authPic, auth, artDate, artLength, artSection}){
 
     return(
@@ -15,9 +97,7 @@ function Article({ artTitle,artDescr, authPic, auth, artDate, artLength, artSect
         <authpic authPic={authPic} />
         <artdate artDate = {artDate}/>
         </>
-
-    
     )
-}
+}*/
 
 export default Article

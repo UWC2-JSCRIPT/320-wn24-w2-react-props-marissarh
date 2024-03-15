9bb356c84074eca3240a0b26d64c19945b762525
marissarh <mbutto26@gmail.com>
2024-03-14 17:25:27 -0700

    update

--- blog-homepage/src/App.jsx
+++ blog-homepage/src/App.jsx
@@ -1,13 +1,53 @@
-import {React,Routes, Route} from 'react';
-import Component from './Component';
+import { React } from 'react';
+import yourArticles from './_data/your-articles.json';
+import missedArticles from './_data/missed-articles.json'
+import Articles from './Articles';
 
 
 function App(){
 
-  return
+  return(
+    <div>
+    <div>
+      <h1>For you</h1>
+      <hr/>
+    </div>
+    <div>
+      {yourArticles.map(article => {
+        <Articles
+
+        title={article.title}
+        description={article.description}
+        image={article.image}
+        link={article.link}
+        author={article.author}
+        postedDate={article.postedDate}
+        minutesToRead={article.minutesToRead}
+        hasAudioAvailable={article.hasAudioAvailable}
+        memberPreview={article.memberPreview}
+        />}
+      )}
+    </div>
+    <h2>In case you missed it</h2>
+    <div>
+    {missedArticles.map(article => {
+        <Articles
+
+        title={article.title}
+        description={article.description}
+        image={article.image}
+        link={article.link}
+        author={article.author}
+        postedDate={article.postedDate}
+        minutesToRead={article.minutesToRead}
+        hasAudioAvailable={article.hasAudioAvailable}
+        memberPreview={article.memberPreview}
+        />})}
+    </div>
+    </div>
+  )
   
 
-  <Component />
   
 }
  export default App

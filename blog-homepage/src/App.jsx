59b70e3b9253ff2096b6b4309cb42ea79b5472c6
marissarh <mbutto26@gmail.com>
2024-03-16 16:00:29 -0700

    update

--- blog-homepage/src/App.jsx
+++ blog-homepage/src/App.jsx
@@ -1,49 +1,25 @@
 import { React } from 'react';
-import yourArticles from './_data/your-articles.json';
-import missedArticles from './_data/missed-articles.json'
-import Articles from './Articles';
+
+
 
 
 function App(){
+  fetch('./_data/your-articles.json')
+  .then(response =>{
+    if(!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+
+  })
+  .then(data =>{
+    console.log(data);
+  });
+  
 
   return(
     <div>
-    <div>
-      <h1>For you</h1>
-      <hr/>
-    </div>
-    <div>
-      {yourArticles.map(article => {
-        <Articles
-
-        title={article.title}
-        description={article.description}
-        image={article.image}
-        link={article.link}
-        author={article.author}
-        postedDate={article.postedDate}
-        minutesToRead={article.minutesToRead}
-        hasAudioAvailable={article.hasAudioAvailable}
-        memberPreview={article.memberPreview}
-        />}
-      )}
-    </div>
-    <h2>In case you missed it</h2>
-    <div>
-    {missedArticles.map(article => {
-        <Articles
-
-        title={article.title}
-        description={article.description}
-        image={article.image}
-        link={article.link}
-        author={article.author}
-        postedDate={article.postedDate}
-        minutesToRead={article.minutesToRead}
-        hasAudioAvailable={article.hasAudioAvailable}
-        memberPreview={article.memberPreview}
-        />})}
-    </div>
+    
     </div>
   )
   
---
+++ blog-homepage/src/Articles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  PropTypes from 'prop-types'
+
+
+function Articles() {
+  return (
+    <div>Articles</div>
+  )
+}
+
+export default Articles

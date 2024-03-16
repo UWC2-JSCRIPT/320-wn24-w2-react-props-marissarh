20bd999ea5faf55a63aa8008f83a1b885894ef11
marissarh <mbutto26@gmail.com>
2024-03-16 13:52:59 -0700

    Create App.jsx

---
+++ blog-homepage/src/App.jsx
@@ -0,0 +1,56 @@
+import { React } from 'react';
+import yourArticles from '.../_data'
+import missedArticles from '.../_data'
+import Articles from './Articles';
+
+
+function App(){
+
+  return(
+    <div>
+    <div>
+      <h1>For you</h1>
+      <hr/>
+    </div>
+    <div>
+      {yourArticles.map(article => {
+        <Articles
+
+        title={article.title}
+        description={article.description}
+        image={article.image}
+        link={article.link}
+        author={article.author}
+        postedDate={article.postedDate}
+        minutesToRead={article.minutesToRead}
+        hasAudioAvailable={article.hasAudioAvailable}
+        memberPreview={article.memberPreview}
+        />}
+      )}
+    </div>
+    <h2>In case you missed it</h2>
+    <div>
+    {missedArticles.map(article => {
+        <Articles
+
+        title={article.title}
+        description={article.description}
+        image={article.image}
+        link={article.link}
+        author={article.author}
+        postedDate={article.postedDate}
+        minutesToRead={article.minutesToRead}
+        hasAudioAvailable={article.hasAudioAvailable}
+        memberPreview={article.memberPreview}
+        />})}
+    </div>
+    </div>
+  )
+  
+
+  
+}
+ export default App
+      
+
+
